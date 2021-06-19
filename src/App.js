c1bbc0121d83a03be80fbca37b9c7bf5883f75c0
irah rosete <irah.rosete@outlook.com>
2021-06-19 15:22:43 +1000

    implement toggle between light and dark modes

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import './app.css'
 
 const Container = styled.div`
   padding: 200px;
-  background-color: #222;
-  color: #eee;
   border-radius: 3px;
   border: 2px solid cornflowerblue;
 `
 
+const dark = {
+  backgroundColor: "#222",
+  color: "#eee"
+}
+
+const light = {
+  backgroundColor: "#eee",
+  color: "#222"
+}
+
 const App = () => {
   const [text, setText] = useState('Light')
+  const [bg, setBg] = useState(light)
 
   const handleClick = () => {
     text === "Light" ? setText('Dark') : setText("Light")
+    bg === dark ? setBg(light) : setBg(dark)
   }
 
   return (
@@ -22,7 +32,7 @@ const App = () => {
     <button type="button" className="btn" onClick={handleClick}>
       switch
     </button>
-    <Container>
+    <Container style={bg}>
       <h1>{text}</h1>
       <h1>Theme</h1>
     </Container>
@@ -31,3 +41,6 @@ const App = () => {
 }
 
 export default App
+
+// FaToggleOff
+// FaToggleOn
